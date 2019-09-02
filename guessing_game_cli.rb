fb154573bfe49a51add1c5e5223345e94c0d242f
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-02 00:48:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,14 +2,16 @@
 run_guessing_game
 
   rand_num = rand 1..6
-
-  puts "Guess a number between 1 and 6"
+  
+  prompt_user
+  
+  user_number = 
 
 end
 
 
 def prompt_user
-  puts "Guess "
+  puts "Guess a number between 1 and 6"
 end
 
 def get_input
